680e4e45d53c4d0c307e14b8233ae2deebfef928
alifakhar2 <alifakhar@bce.au.dk>
2025-01-17 11:42:05 +0100

    VOC

--- DFC VOC/VOCs.R
+++ DFC VOC/VOCs.R
@@ -1,13 +1,35 @@
-library(readxl)
-library(dplyr)
-library(lubridate)
-library(ggplot2)
 
-# List of file paths
+#Libraries used to run script#
+
+library(data.table)
+library(lubridate)
+library(dplyr)
+library(stringr)
+library(zoo)
+library(ggplot2)
+library(readxl)
+
+################################################################################################################################
+#Prerequ#Prerequ#Prerequisites to run readCRDS function#
+devtools::source_url('https://raw.githubusercontent.com/AU-BCE-EE/guidance/main/Picarro/PicarroFunction.R')
+
+#Reading in Picarro data#
+da <- readCRDS(('DFC_Picarro'), From = '18.09.2024 06:00:40', To = '24.09.2024 08:00:00', mult = F, tz = "ETC/GMT-1", rm = F)
+
+################################################################################################################################
+# Making date.time stamp#
+da$date.time <- paste(da$DATE, da$TIME)
+da$date.time <- ymd_hms(da$date.time)
+da$st <- da$date.time                                       
+da$DATE <- as.Date(da$st)                                 
+da$TIME <- format(da$st, format = "%H:%M:%S") 
+################################################################################################################################
+
+# List of file paths VOCs
 files <- c("VOC Data/File 1.xlsx", "VOC Data/File 2.xlsx", "VOC Data/File 3.xlsx")
 
 # Read and combine data from each file
-da <- lapply(files, function(file) {
+vocs <- lapply(files, function(file) {
   # Read the file and sheet
   data <- read_excel(file, sheet = "Conc_ppb")
   
@@ -15,38 +37,57 @@ da <- lapply(files, function(file) {
 })
 
 # Combine all data into a single data frame
-data <- bind_rows(da)
+voc <- bind_rows(vocs)
 
-
-
-data$date.time <- paste(data$`Absolute Time`)
-data$date.time <- ymd_hms(data$date.time)
-data$date.time <- with_tz(data$date.time, tz = "ETC/GMT-1")  # Adjust to Ecuador Time (ECT)
-#Removing unnecessary data#
-data <- data[, -c(1, 2, 24:45)]
+#Time stamp
+voc$date.time.v <- paste(voc$`Absolute Time`)
+voc$date.time.v <- ymd_hms(voc$date.time.v)
+voc$date.time.v <- with_tz(voc$date.time.v, tz = "ETC/GMT-1")  
 
 # Define the start and end
-start_time <- ymd_hms("2024-09-18 11:30:00")
-end_time <- ymd_hms("2024-09-24 6:59:00")
+start_time <- ymd_hms("2024-09-18 05:00:40")
+end_time <- ymd_hms("2024-09-24 07:00:00")
 # Filter the data based on the new timezone
-fdata <- data %>%
-  filter(date.time >= start_time & date.time <= end_time)
+voc <- voc %>%
+  filter(date.time.v >= start_time & date.time.v <= end_time)
 
-fdata$date.time <- floor_date(fdata$date.time, unit = "hour")
-
-
-################################################################################################################################
+#Removing unnecessary data#
+voc<- voc[, -c(1, 2, 24:45)]
 
 
-# Read the CSV file containing the NH3 data (MPV position)
-mpv_data <- read.csv('mpv.csv')
+#Merging Data
+da$date.time <- as.POSIXct(da$date.time, format = '%Y-%m-%d %H:%M', tz = "ETC/GMT-1")
+voc$date.time.v <- as.POSIXct(voc$date.time.v, format = '%Y-%m-%d %H:%M', tz = "ETC/GMT-1")
+da$date.time <- floor_date(da$date.time, unit = "hour")
+voc$date.time.v <- floor_date(voc$date.time.v, unit = "hour")
 
-mpv_data$date.time <- paste(mpv_data$st)
-mpv_data$date.time <- ymd_hms(mpv_data$date.time)
-mpv_data$date.time <- with_tz(mpv_data$date.time, tz = "ETC/GMT-1")  # Adjust to Ecuador Time (ECT)
-mpv_data$date.time <- floor_date(mpv_data$date.time, unit = "hour")
+mdata <- left_join(da, voc, by = c('date.time' = 'date.time.v'))
 
-################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
