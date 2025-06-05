ff14c96174e1ba37e048df489e5e7238e36418d2
Flavia Dela Pierre <flavia.delapierre@unito.it>
2025-06-05 17:24:37 +0200

    Update VOC Flux.R

--- Scripts DFC_VOC/VOC Flux.R
+++ Scripts DFC_VOC/VOC Flux.R
@@ -14,7 +14,7 @@ A.frame <- (0.7/2)^2 * 3.14  # Chamber area (m^2)
 weather <- read_csv("../Flavia_VOC_DFC_data/Temp.csv") #find this in Flavia_VOC_DFC_data folder
 
 # Import VOC data, the 30s background corrected averages created in the VOC concentrations and OAV.R
-dat <- read_delim("../Flavia_VOC_DFC_data/voc.30s.corrected.csv", 
+dat <- read_delim("../Flavia_VOC_DFC_data/voc.30s.corrected.txt", 
                   delim = ",", escape_double = FALSE, trim_ws = TRUE)
 vsc_ini <- read_csv("../Flavia_VOC_DFC_data/vsc.first.minutes.txt") #file created in the VOC concentrations and OAV script, these are the initial concentrations for VSC (first 8 minutes)
 #remove any duplicate rows in dat, otherwise mintegrate does not work
